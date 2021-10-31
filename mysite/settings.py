067a604837ea054d346c071950c8deac5f256f58
superherodemigod <blackeagle0188@gmail.com>
2021-10-31 10:24:49 +0900

    update setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,10 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
